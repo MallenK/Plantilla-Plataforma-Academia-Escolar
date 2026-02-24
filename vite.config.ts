b7eca2ad22a39b27e49d3dfeba6ce3808d5862d0
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2026-02-24 13:08:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,27 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
+
   return {
-    base: '/',   // ← necesario para GitHub Pages
-    
+    base: '/Plataforma-Academia-Escolar/', // 👈 nombre EXACTO del repo
+
     plugins: [react(), tailwindcss()],
+
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
+
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
+
     server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
